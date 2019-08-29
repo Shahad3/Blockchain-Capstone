6cff86b46cd582066845574f262d3adf7129da9f
Shahad Alsaedi <48574016+Shahad3@users.noreply.github.com>
2019-08-29 13:41:54 +0300

    Add files via upload

--- eth-contracts/truffle-config.js
+++ eth-contracts/truffle-config.js
@@ -80,6 +80,7 @@ module.exports = {
   // Set default mocha options here, use special reporters etc.
   mocha: {
     // timeout: 100000
+    enableTimeouts: false
   },
 
   // Configure your compilers
