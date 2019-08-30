ce54587f2939193e08991a8023e10b7d7c62752e
Shahad Alsaedi <48574016+Shahad3@users.noreply.github.com>
2019-08-30 16:48:29 +0300

    Update minting.js

--- eth-contracts/test/minting.js
+++ eth-contracts/test/minting.js
@@ -1,59 +1,47 @@
-// import Web3 from 'web3';
-// import SolnSquareVerifierArtifact from "../build/contracts/SolnSquareVerifier.json";
+const HDWalletProvider = require("truffle-hdwallet-provider")
+const web3 = require('web3');
+const MNEMONIC = "";
+const INFURA_KEY = ""
+
+const CONTRACT_ADDRESS = "0x720550B2c8c34D586f5B4158816f74e858dfb3ef"
 var SolnSquareVerifierArtifact = require("../build/contracts/SolnSquareVerifier.json");
-const Web3 = require("Web3");
+// const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS
+const OWNER_ADDRESS = "0x5e2CD8Dd1F302C36CbA50b30827CBf5E49907162"
+const NETWORK = "rinkeby";
 var proofs = require('../proofs.js');
 
-const App = {
-    web3: null,
-    account: null,
-    meta: null,
-  
-    start: async function() {
-      const { web3 } = this;
-  
-      try {
-        // get contract instance
-        const networkId = await web3.eth.net.getId();
-        const deployedNetwork = SolnSquareVerifierArtifact.networks[networkId];
-        this.meta = new web3.eth.Contract(
-        SolnSquareVerifier.abi,
-          deployedNetwork.address,
-        );
 
-        // get accounts
-      const accounts = await web3.eth.getAccounts();
-      this.account = accounts[0];
-    } catch (error) {
-        console.error("Could not connect to contract or chain.");
-      }
-    },
-    mint: async function() {
-        const { mint } = this.meta.methods;
+if (!MNEMONIC || !INFURA_KEY || !OWNER_ADDRESS || !NETWORK) {
+    console.error("Please set a mnemonic, infura key, owner, network, and contract address.")
+    return
+}
+
+// const NFT_ABI = 
+const abi = SolnSquareVerifierArtifact.abi;
+
+async function main() {
+    const provider = new HDWalletProvider(MNEMONIC, `https://${NETWORK}.infura.io/v3/${INFURA_KEY}`)
+    const web3Instance = new web3(
+        provider
+    )
+try{
+    if (CONTRACT_ADDRESS) {
+        const nftContract = new web3Instance.eth.Contract(abi, CONTRACT_ADDRESS, { gasLimit: "1000000" })
+
         for(var i=0; i<10; i++){
-            let proof = proofs[i];
-        await mint(this.account, i+1, proof.proof.a, proof.proof.b, proof.proof.c, proof.inputs).send({from: this.account});
-        console.log("I am here ..")
-        }
-      },
+          let proof = proofs[i];
+      var result = await nftContract.methods.mint(OWNER_ADDRESS, i+1, proof.proof.a, proof.proof.b, proof.proof.c, proof.inputs).send({from: OWNER_ADDRESS});
+      console.log("I am here ..")
+      }
 
-    };
-
+        // Creatures issued directly to the owner.
+        // for (var i = 0; i < NUM_CREATURES; i++) {
+        //     const result = await nftContract.methods.mintTo(OWNER_ADDRESS).send({ from: OWNER_ADDRESS });
+        //     console.log("Minted creature. Transaction: " + result.transactionHash)
+        // }
+    } 
+  }catch(e){console.log(e," |||| ", e.message);}
     
+}
 
-window.App = App;
-
-window.addEventListener("load", async function() {
-  if (window.ethereum) {
-    // use MetaMask's provider
-    App.web3 = new Web3(window.ethereum);
-    await window.ethereum.enable(); // get permission to access accounts
-  } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
-  }
-
-  App.start();
-  App.mint();
-});
+main()
